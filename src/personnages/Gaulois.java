7c361abc11978a0d4f1516145f8d6b0678661694
dubor damien <dbd4163a@ens.fsi>
2022-09-26 19:44:53 +0200

    save 19h44

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,49 @@
+package personnages;
+
+public class Gaulois {
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	public int getForce() {
+		return force;
+	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	public static void main(String[] args) {
+		if (condition) {
+			
+		} else {
+
+		}
+		switch (key) {
+		case value:
+			
+			break;
+
+		default:
+			break;
+		}
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+		do {
+			
+		} while (condition);
+		public void parler(String texte) {
+			prendreParole();
+			
+		}
+	}
+	private static void prendreParole() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
